2c3487423b7ebfea82228297dba91a712419f293
Efe Ali Özkesici <efealiozkesici@hotmail.com>
2019-04-05 23:39:27 +0300

    Observer package defined.

---
+++ src/Observer/NotifyAction.java
@@ -0,0 +1,5 @@
+package Observer;
+
+public enum NotifyAction {
+    INITIAL_STATE, START, FINISH
+}
---
+++ src/Observer/Subject.java
@@ -0,0 +1,53 @@
+package Observer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subject {
+    private List<Observer> observers = new ArrayList<Observer>();
+    private boolean changed; // Is tehere any update?
+
+    public void addObserver(Observer o){
+        if(o == null){
+            throw new NullPointerException();
+
+        }
+        if(!observers.contains(o)){
+            observers.add(o);
+        }
+    }
+
+    public void deleteObserver(Observer o){
+        observers.remove(o);
+    }
+
+    public void clearChanged(){
+        changed = false;
+    }
+
+    public void setChanged(){
+        changed = true;
+    }
+
+    public boolean hasChanged(){
+        return changed;
+    }
+
+    public void clearObservers() {
+        observers.clear();
+    }
+
+    public void notifyAllObservers() {
+        notifyAllObservers(null);
+    }
+
+    public void notifyAllObservers(NotifyAction action){
+        if(!hasChanged()){
+            return;
+        }
+        clearChanged();
+
+        for (int i = observers.size() - 1; i >= 0; i--) {
+            (observers.get(i)).update(this, action);
+        }
+    }
+}
