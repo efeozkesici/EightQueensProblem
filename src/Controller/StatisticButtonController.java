be048df42cddab5a18c222e10fc620938a19a89a
Efe Ali Özkesici <efealiozkesici@hotmail.com>
2019-04-06 13:05:47 +0300

    StatisticButtonController was defined.

---
+++ src/Controller/StatisticButtonController.java
@@ -0,0 +1,18 @@
+package Controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+public class StatisticButtonController implements ActionListener {
+    private JFrame statisticFrame;
+
+    public void init(JFrame statisticFrame) {
+        this.statisticFrame = statisticFrame;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent arg0) {
+        this.statisticFrame.setVisible(true);
+    }
+}
