c799d5942272a6bb9e8067005bc9d57001987130
Efe Ali Özkesici <efealiozkesici@hotmail.com>
2019-04-06 23:19:46 +0300

    AbstractGUIFactory was defined.

--- src/Factory/AbstractGUIFactory.java
+++ src/Factory/AbstractGUIFactory.java
@@ -1,4 +1,18 @@
 package Factory;
 
+import javax.swing.JFrame;
+import Controller.StatisticButtonController;
+import Controller.SolveButtonController;
+import View.ButtonPanel;
+import View.ChessPanel;
+import View.StatisticPanel;
+
 public interface AbstractGUIFactory {
+    public JFrame createEightQueenFtame();
+    public ChessPanel createChessPanel();
+    public ButtonPanel createButtonPanel();
+    public SolveButtonController createSolveButtonController();
+    public StatisticButtonController createStatisticButtonController();
+    public JFrame createStatisticFrame();
+    public StatisticPanel createStatisticPanel();
 }
