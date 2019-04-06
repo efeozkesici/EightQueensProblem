68f84f323eb8fb36c14c3b7889a3ac725ca28210
Efe Ali Özkesici <efealiozkesici@hotmail.com>
2019-04-06 12:59:13 +0300

    ProblemSolver model was defined.

--- src/Model/ProblemSolver.java
+++ src/Model/ProblemSolver.java
@@ -7,4 +7,33 @@ import Observer.Subject;
 public class ProblemSolver extends Subject {
     HillClimbing hillClimbing;
 
+    public int[] getCurrentPositions(){
+        return this.hillClimbing.getCurrentPositions();
+    }
+
+    public ProblemSolver(){
+        hillClimbing = new HillClimbing();
+    }
+
+    public void solveProblem(){
+        hillClimbing.init();
+
+        this.setChanged();
+        this.notifyAllObservers(NotifyAction.START);
+        hillClimbing.runHillClimbing(this);
+        this.setChanged();
+        this.notifyAllObservers(NotifyAction.FINISH);
+    }
+
+    public int getRandomRestartCount(){
+        return this.hillClimbing.getRandomRestartCount();
+    }
+
+    public int getMoveCount(){
+        return this.hillClimbing.getMoveCount();
+    }
+
+    public double getProcessTime(){
+        return this.hillClimbing.getProcessTime();
+    }
 }
