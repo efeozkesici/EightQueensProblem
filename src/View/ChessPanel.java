d076176bed9b6ff1c016e76c98920cc2df53ac39
Efe Ali Özkesici <efealiozkesici@hotmail.com>
2019-04-06 13:52:16 +0300

    ChessPanel, Field and StatisticPanel was defined.

--- src/View/ChessPanel.java
+++ src/View/ChessPanel.java
@@ -1,4 +1,65 @@
 package View;
 
-public class ChessPanel {
+import java.awt.Color;
+import java.awt.GridLayout;
+import javax.swing.JPanel;
+import Model.ProblemSolver;
+import Observer.NotifyAction;
+import Observer.Subject;
+import Observer.Observer;
+
+public class ChessPanel extends JPanel implements Observer{
+
+    private static final long serialVersionUID = -3750323525250432419L;
+    private final Field[][] fields;
+    private JPanel panel = new JPanel(new GridLayout(8, 8));
+
+    public ChessPanel() {
+        this.add(panel);
+        this.fields = new Field[8][8];
+        for (int r = 0; r < 8; r++) {
+            for (int c = 0; c < 8; c++) {
+                if (r % 2 == 0 && c % 2 == 0 || r % 2 == 1 && c % 2 == 1) {
+                    this.fields[r][c] = new Field(Color.WHITE);
+                } else {
+                    this.fields[r][c] = new Field(Color.BLACK);
+                }
+                panel.add(this.fields[r][c]);
+            }
+        }
+    }
+
+    public void showSolution(final int[] positions) {
+        for (int c = 0; c < positions.length; c++) {
+            int r = positions[c];
+            this.fields[r][c].putQueen();
+        }
+    }
+
+    public void clearBoard() {
+        for (int r = 0; r < 8; r++) {
+            for (int c = 0; c < 8; c++) {
+                fields[r][c].removeQueen();
+            }
+        }
+    }
+
+    @Override
+    public void update(Subject s, NotifyAction action) {
+        switch (action) {
+            case START:
+                updateBoard((ProblemSolver) s);
+                break;
+            case FINISH:
+                clearBoard();
+                updateBoard((ProblemSolver) s);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void updateBoard(ProblemSolver s) {
+        showSolution(s.getCurrentPositions());
+    }
 }
