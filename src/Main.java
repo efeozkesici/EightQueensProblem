46e8cad9c14b174241764aef6189cf2a0ec651c6
Efe Ali Özkesici <efealiozkesici@hotmail.com>
2019-04-08 22:29:48 +0300

    Main was defined

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import javax.swing.UIManager;
+import Factory.EightQueenClient;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        new EightQueenClient();
     }
 }
